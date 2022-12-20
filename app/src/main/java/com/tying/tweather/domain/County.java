b1740b50efa10e058814f620cb6824b33ddee67e
Tying <542923663@qq.com>
2022-12-20 12:57:05 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/tying/tweather/domain/County.java
@@ -0,0 +1,49 @@
+package com.tying.tweather.domain;
+
+import org.litepal.annotation.Column;
+import org.litepal.exceptions.DataSupportException;
+
+public class County extends DataSupportException {
+
+    public County(String errorMessage) {
+        super(errorMessage);
+    }
+
+    @Column(unique = true, nullable = true)
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/tying/tweather/domain/Province.java
@@ -0,0 +1,40 @@
+package com.tying.tweather.domain;
+
+import org.litepal.annotation.Column;
+import org.litepal.exceptions.DataSupportException;
+
+public class Province extends DataSupportException {
+
+    public Province(String errorMessage) {
+        super(errorMessage);
+    }
+
+    @Column(unique = true, nullable = false)
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
